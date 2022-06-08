4b9d6b502b8711af04d732b6aa3864d9d030ed78
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-06-08 18:07:10 -0300

    How to create the register form with PHP and PDO

--- cadastrar.php
+++ cadastrar.php
@@ -14,17 +14,48 @@ include_once './conexao.php';
 
         if(!empty($dados['cadUser'])){
             var_dump($dados);
-            $query_usuario = "INSERT INTO usuarios (nome, email) VALUES ('" . $dados['nome'] . "', '". $dados['email'] ."')";
-            $cad_usuario = $conn->prepare($query_usuario);
-            $cad_usuario->execute();
+            $empty_input = false;
+
+            $dados = array_map('trim', $dados);
+            if(in_array("", $dados)){
+                $empty_input = true;
+                echo "<p style='color: #f00;'>Erro: Necessário preencher todos campos!</p>";
+                
+            }elseif(!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)){
+                $empty_input = true;
+                echo "<p style='color: #f00;'>Erro: Necessário preencher com e-mail válido!</p>";
+            }
+            
+            if(!$empty_input){
+            
+                $query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email)";
+                $cad_usuario = $conn->prepare($query_usuario);
+                $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+                $cad_usuario->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+                $cad_usuario->execute();
+            if($cad_usuario->rowCount()){
+                echo "<p style='color: green;'>Usuário cadastrado com sucesso!</p>";
+                unset($dados);
+            }else{
+                echo "<p style='color: #f00;'>Erro: Usuário não cadastrado!</p>";
+            }
+            }
         }
     ?>
     <form name="cad-user" method="POST" action="">
         <label>Nome: </label>
-        <input type="text" name="nome" id="nome" placeholder="Nome completo"><br><br>
+        <input type="text" name="nome" id="nome" placeholder="Nome completo" value="<?php
+            if(isset($dados['nome'])){
+                echo $dados['nome'];
+            }
+        ?>"><br><br>
         
         <label>E-mail: </label>
-        <input type="email" name="email" id="email" placeholder="Seu melhor e-mail"><br><br>
+        <input type="email" name="email" id="email" placeholder="Seu melhor e-mail"value="<?php
+            if(isset($dados['email'])){
+                echo $dados['email'];
+            }
+        ?>"><br><br>
 
         <input type="submit" value="cadastrar" name="cadUser">
     </form>
