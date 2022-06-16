c5a7e734c290bb2654a9cd4b4c237803a3bcaacf
Wagner Andrade <wagnerandrade_df@hotmail.com>
2022-06-16 10:06:16 -0300

    Saving email message in Database

--- conexao.php
+++ conexao.php
@@ -7,7 +7,14 @@ $dbname = "crudphp";
 $port = 3306;
 
 //Conexão com a porta
-$conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+try{
+
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname, $user, $pass);
+
+} catch (Exception $ex){
+    echo "Erro: Conexão com banco de dados não realizada!";
+}
+
 
 //Conexão sem a porta
 //$conn = new PDO("mysql:host=$host;dbname=".$dbname, $user, $pass);
--- mensagem.php
+++ mensagem.php
@@ -21,7 +21,22 @@ include_once './nav.php';
     <?php
     
         $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        echo "<pre>", var_dump($data), "</pre>";
+        // echo "<pre>", var_dump($data), "</pre>";
+        $query_msg = "INSERT INTO contact_msgs (name, email, subject, content, created) VALUES (:name, :email, :subject, :content, NOW())";
+        $add_msg = $conn->prepare($query_msg);
+
+        $add_msg->bindParam(':name', $data['name'], PDO::PARAM_STR);
+        $add_msg->bindParam(':email', $data['email'], PDO::PARAM_STR);
+        $add_msg->bindParam(':subject', $data['subject'], PDO::PARAM_STR);
+        $add_msg->bindParam(':content', $data['content'], PDO::PARAM_STR);
+
+        $add_msg->execute();
+
+        if($add_msg->rowCount()){
+            echo "<p style='color: green;'>Mensagem de contato enviada com sucesso!</p>";
+        }else{
+            echo "<p style='color: #f00;'>Erro: Mensagem de contato não pode ser enviada!</p>";
+        }
     
     ?>
 
@@ -29,35 +44,35 @@ include_once './nav.php';
 
     <form name="add_msg" action="" method="POST">
         
-    <div class="form-group row col-md-4 mb-3" >
+    <div class="form-group row col-md-5 mb-3" >
         <label for="inputNome" class="col-sm-2 col-form-label">Nome: </label>
         <div class="col-sm-10">
             <input type="text" name="name" class="form-control" id="name" placeholder="Nome completo" required>
         </div>
     </div>
 
-    <div class="form-group row col-md-4 mb-3" >
+    <div class="form-group row col-md-5 mb-3" >
         <label for="inputEmail" class="col-sm-2 col-form-label">E-mail: </label>
         <div class="col-sm-10">
             <input type="email" name="email" class="form-control" id="email" placeholder="O melhor e-mail" required>
         </div>
     </div>
 
-    <div class="form-group row col-md-4 mb-3" >
+    <div class="form-group row col-md-5 mb-3" >
         <label for="inputAssunto" class="col-sm-2 col-form-label">Assunto: </label>
         <div class="col-sm-10">
             <input type="text" name="subject" class="form-control" id="subject" placeholder="Assunto da mensagem" required>
         </div>
     </div>
 
-    <div class="form-group row col-md-4 mb-3" >
+    <div class="form-group row col-md-5 mb-3" >
         <label for="inputConteudo" class="col-sm-2 col-form-label">Conteúdo: </label>
         <div class="col-sm-10">
             <input type="text" name="content" class="form-control" id="content" placeholder="Conteudo da mensagem" required>
         </div>
     </div>
 
-    <div class="form-group row col-md-4 mb-3" >
+    <div class="form-group row col-md-5 mb-3" >
     <button type="submit" name="SendAddMsg" class="btn btn-primary">Enviar</button>
     </div>
 
